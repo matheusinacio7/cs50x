907f60c88939743cd0657491ed10871bb1b5fa97
heyset <c.inaciomatheus@gmail.com>
2022-05-18 04:48:16 -0300

    week-1: cash: adds remaining functions

--- week-1-c/problem-set-1/cash/cash.c
+++ week-1-c/problem-set-1/cash/cash.c
@@ -50,24 +50,20 @@ int get_cents(void)
 
 int calculate_quarters(int cents)
 {
-    // TODO
-    return 0;
+    return cents / 25;
 }
 
 int calculate_dimes(int cents)
 {
-    // TODO
-    return 0;
+    return cents / 10;
 }
 
 int calculate_nickels(int cents)
 {
-    // TODO
-    return 0;
+    return cents / 5;
 }
 
 int calculate_pennies(int cents)
 {
-    // TODO
-    return 0;
+    return cents;
 }
