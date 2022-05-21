bce64e4a5fa4d80970687acc2ff2eabd5b0c8291
heyset <c.inaciomatheus@gmail.com>
2022-05-21 17:01:42 -0300

    week-2: pset: adds function that calculates the index

--- week-2-arrays/pset/readability/readability.c
+++ week-2-arrays/pset/readability/readability.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_coleman_liau_index(float L, float S);
+
 int main(void) 
 {
+    int index = get_coleman_liau_index(464.29, 28.57);
+}
 
+// Where L is the average of letters per 100 words in the text, and S is the average of sentences per 100 words in the text
+int get_coleman_liau_index(float L, float S)
+{
+    return (int) (0.0588 * L - 0.296 * S - 15.8);
 }
