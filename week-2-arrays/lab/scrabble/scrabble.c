9ccd3d669cdfdb18bd1fef885fba49034dded343
heyset <c.inaciomatheus@gmail.com>
2022-05-21 16:36:18 -0300

    week-2: lab: adds winner printing

--- week-2-arrays/lab/scrabble/scrabble.c
+++ week-2-arrays/lab/scrabble/scrabble.c
@@ -7,6 +7,7 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
+void print_winner(int score1, int score2);
 
 int main(void)
 {
@@ -18,8 +19,8 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
-    return 3;
+    print_winner(score1, score2);
+    printf("\n");
 }
 
 int compute_score(string word)
@@ -33,4 +34,20 @@ int compute_score(string word)
         sum += letter_score;
     }
     return sum;
+}
+
+void print_winner(int score1, int score2)
+{
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
 }
