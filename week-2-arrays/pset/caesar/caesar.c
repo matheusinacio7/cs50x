a0b9e2e51d29e4eb74a88d1b29b64262af53e23d
heyset <c.inaciomatheus@gmail.com>
2022-05-23 16:09:24 -0300

    week-2: caesar: adds rotate letter function

--- week-2-arrays/pset/caesar/caesar.c
+++ week-2-arrays/pset/caesar/caesar.c
@@ -7,21 +7,45 @@
 void print_usage_reminder(void);
 int string_to_int(string number);
 int char_to_int(char number);
+char rotate_letter(char letter, int key);
 
 int main(int argc, string argv[]) 
 {
-    int my_int = string_to_int("12489");
+    char rotated = rotate_letter('B', 5);
 
     if (argc != 2)
     {
         print_usage_reminder();
         return 1;
     }
+
+    int key = string_to_int(argv[0]);
 }
 
 void print_usage_reminder(void)
 {
     printf("Usage: ./caesar key\n");
+}
+
+char rotate_letter(char letter, int key)
+{
+    int code = (int) letter;
+    int upper_case_range[] = { 65, 90 };
+    int lower_case_range[] = { 97, 122 };
+
+    if (code < upper_case_range[0] || code > lower_case_range[1])
+    {
+        return letter;
+    }
+    else if (code > upper_case_range[1] && code < lower_case_range[0])
+    {
+        return letter;
+    }
+    else
+    {
+        int new_code = code + key % 26;
+        return (char) new_code;
+    }
 }
 
 int string_to_int(string number)
