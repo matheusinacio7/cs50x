d40b8b4771116720d5458ced391cb7dba836e0b7
heyset <c.inaciomatheus@gmail.com>
2022-05-23 16:16:03 -0300

    week-2: caesar: adds string fetching

--- week-2-arrays/pset/caesar/caesar.c
+++ week-2-arrays/pset/caesar/caesar.c
@@ -7,24 +7,34 @@
 void print_usage_reminder(void);
 int string_to_int(string number);
 int char_to_int(char number);
+void rotate_phrase(string phrase, int key);
 char rotate_letter(char letter, int key);
 
 int main(int argc, string argv[]) 
 {
-    char rotated = rotate_letter('B', 5);
-
     if (argc != 2)
     {
         print_usage_reminder();
         return 1;
     }
 
-    int key = string_to_int(argv[0]);
+    int key = string_to_int(argv[1]);
+    string text = get_string("plaintext:  ");
+    rotate_phrase(text, key);
+    printf("\nciphertext: %s\n", text);
 }
 
 void print_usage_reminder(void)
 {
     printf("Usage: ./caesar key\n");
+}
+
+void rotate_phrase(string phrase, int key)
+{
+    for (int i = 0; i < strlen(phrase); i++)
+    {
+        phrase[i] = rotate_letter(phrase[i], key);
+    }
 }
 
 char rotate_letter(char letter, int key)
