d1e5242fb0b2b5c2dcc497aa15577de51c72de8c
heyset <c.inaciomatheus@gmail.com>
2022-05-24 02:27:24 -0300

    week-2: substitution: adds duplicate validation

--- week-2-arrays/pset/substitution/substitution.c
+++ week-2-arrays/pset/substitution/substitution.c
@@ -6,6 +6,7 @@
 
 void print_usage_reminder(void);
 void extract_cipher(string raw_cipher, char lower_case_cipher[26], char upper_case_cipher[26]);
+void validate_cipher(string raw_cipher);
 void encrypt(string plain, char lower_case_cipher[26], char upper_case_cipher[26]);
 
 int main(int argc, string argv[]) 
@@ -36,7 +37,9 @@ int main(int argc, string argv[])
 void print_usage_reminder(void)
 {
     printf("Usage: ./substitution cipher\n");
-    printf("  cipher: 26 letters that letters a-z will be substituted for. Case insensitive.\n");
+    printf("  cipher: 26 letters that letters a-z will be substituted for.\n");
+    printf("     Case insensitive.\n");
+    printf("     Must contain exactly one of each letter.\n");
 }
 
 void extract_cipher(string raw_cipher, char lower_case_cipher[26], char upper_case_cipher[26])
@@ -51,6 +54,39 @@ void extract_cipher(string raw_cipher, char lower_case_cipher[26], char upper_ca
         }
         lower_case_cipher[i] = tolower(raw_cipher_char);
         upper_case_cipher[i] = toupper(raw_cipher_char);
+    }
+    validate_cipher(lower_case_cipher);
+}
+
+void validate_cipher(char lower_case_cipher[26])
+{
+    char sorted_cipher[26] = { 'a' };
+    for (int i = 0; i < 26; i++)
+    {
+        sorted_cipher[i] = lower_case_cipher[i];
+    }
+
+    // bubble sort
+    for (int i = 0; i < 26; i++)
+    {
+        for (int j = i + 1; j < 26; j++)
+        {
+            if (sorted_cipher[j] < sorted_cipher[i])
+            {
+                char swap = sorted_cipher[j];
+                sorted_cipher[j] = sorted_cipher[i];
+                sorted_cipher[i] = swap;
+            }
+        }
+    }
+
+    for (int i = 0; i < 25; i++)
+    {
+        if (sorted_cipher[i] == sorted_cipher[i + 1])
+        {
+            print_usage_reminder();
+            exit(1);
+        }
     }
 }
 
