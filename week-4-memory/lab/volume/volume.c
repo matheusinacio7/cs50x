d9567373ef3ba6861320ee547f3f9e397fe27739
heyset <c.inaciomatheus@gmail.com>
2022-05-29 19:06:19 -0300

    week-4: lab: adds volume

--- week-4-memory/lab/volume/volume.c
+++ week-4-memory/lab/volume/volume.c
@@ -33,9 +33,20 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    // copy header
+    uint8_t header_byte;
+    for (int i = 0; i < HEADER_SIZE; i++)
+    {
+        fread(&header_byte, sizeof(uint8_t), 1, input);
+        fwrite(&header_byte, sizeof(uint8_t), 1, output);
+    }
 
-    // TODO: Read samples from input file and write updated data to output file
+    int16_t sample;
+    while(fread(&sample, sizeof(int16_t), 1, input))
+    {
+        sample *= factor;
+        fwrite(&sample, sizeof(int16_t), 1, output);
+    }
 
     // Close files
     fclose(input);
