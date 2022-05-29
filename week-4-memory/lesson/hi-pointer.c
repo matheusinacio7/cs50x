d845f53a6a8aedfcb8d7f015adc3fb3b1d2bbaf8
heyset <c.inaciomatheus@gmail.com>
2022-05-29 16:40:52 -0300

    week-4: lesson: peels away square bracket notation

--- week-4-memory/lesson/hi-pointer.c
+++ week-4-memory/lesson/hi-pointer.c
@@ -3,11 +3,11 @@
 int main(void)
 {
     char *s = "HI!";
-    printf("%p -> %i\n", &s[0], *&s[0]);
-    printf("%p -> %i\n", &s[1], *&s[1]);
-    printf("%p -> %i\n", &s[2], *&s[2]);
-    printf("%p -> %i\n", &s[3], *&s[3]);
-    printf("%p -> %i\n", &s[4], *&s[4]);
-    printf("%p -> %i\n", &s[5], *&s[5]);
-    printf("%p -> %i\n", &s[6], *&s[6]);
+    printf("%p -> %i\n", s, *s);
+    printf("%p -> %i\n", s + 1, *(s + 1));
+    printf("%p -> %i\n", s + 2, *(s + 2));
+    printf("%p -> %i\n", s + 3, *(s + 3));
+    printf("%p -> %i\n", s + 4, *(s + 4));
+    printf("%p -> %i\n", s + 5, *(s + 5));
+    printf("%p -> %i\n", s + 6, *(s + 6));
 }
