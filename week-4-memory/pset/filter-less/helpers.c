1278062c3f6ac307f29273a49763ead69b1500ee
heyset <c.inaciomatheus@gmail.com>
2022-05-29 19:52:28 -0300

    week-4: filter-less: adds greyscale

--- week-4-memory/pset/filter-less/helpers.c
+++ week-4-memory/pset/filter-less/helpers.c
@@ -1,8 +1,21 @@
+#include <math.h>
+#include <stdio.h>
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            int sum = image[h][w].rgbtBlue + image[h][w].rgbtGreen + image[h][w].rgbtRed;
+            int average = round(sum / 3.0);
+            image[h][w].rgbtBlue = average;
+            image[h][w].rgbtGreen = average;
+            image[h][w].rgbtRed = average;
+        }
+    }
     return;
 }
 
