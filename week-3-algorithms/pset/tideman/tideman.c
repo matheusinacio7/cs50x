534b6e69c7179486877935bab0ecf93dd1e284b1
heyset <c.inaciomatheus@gmail.com>
2022-05-29 02:47:22 -0300

    week-3: tideman: adds record_preferences

--- week-3-algorithms/pset/tideman/tideman.c
+++ week-3-algorithms/pset/tideman/tideman.c
@@ -114,7 +114,13 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            preferences[ranks[i]][ranks[j]] += 1;
+        }
+    }
     return;
 }
 
