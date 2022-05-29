d7762b1a9d0518adff494ce94f196ce413e1df04
heyset <c.inaciomatheus@gmail.com>
2022-05-29 02:17:38 -0300

    week-3: runoff: fixes is_tie for case where some are eliminated

--- week-3-algorithms/pset/runoff/runoff.c
+++ week-3-algorithms/pset/runoff/runoff.c
@@ -193,7 +193,7 @@ bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes != min)
+        if (!candidates[i].eliminated && candidates[i].votes != min)
         {
             return false;
         }
