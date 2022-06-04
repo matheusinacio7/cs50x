d1932be22e8081be4d5b0b7cd2ee914cbf1ec17b
heyset <c.inaciomatheus@gmail.com>
2022-06-04 15:30:56 -0300

    week-4: filter-more: adds kernel selection

--- week-4-memory/pset/filter-more/helpers.c
+++ week-4-memory/pset/filter-more/helpers.c
@@ -3,9 +3,28 @@
 #include <stdlib.h>
 #include "helpers.h"
 
+int default_kernel[3][3] =
+{
+    { 1, 1, 1 },
+    { 1, 1, 1 },
+    { 1, 1, 1 },
+};
+int gx_kernel[3][3] =
+{
+    { -1, 0, 1 },
+    { -2, 0, 2 },
+    { -1, 0, 1 },
+};
+int gy_kernel[3][3] =
+{
+    { -1, -2, -1 },
+    { 0, 0, 0 },
+    { 1, 2, 1 },
+};
+
 BYTE clamp_color(int target);
 RGBTRIPLE get_box_average(int height, int width, int center_h, int center_w, RGBTRIPLE image[height][width],
-                          RGBTRIPLE original_image[height][width]);
+                          char weight_kernel);
 void copy_image(int height, int width, RGBTRIPLE original[height][width], RGBTRIPLE copy[height][width]);
 
 // Convert image to grayscale
@@ -51,38 +70,43 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int w = 0; w < width; w++)
         {
-            image[h][w] = get_box_average(height, width, h, w, image, original_image);
+            image[h][w] = get_box_average(height, width, h, w, original_image, 'd');
         }
     }
     free(original_image);
     return;
 }
 
+// weight_kernel is d for default, x for gx and y for gy
 RGBTRIPLE get_box_average(int height, int width, int center_h, int center_w, RGBTRIPLE image[height][width],
-                          RGBTRIPLE original_image[height][width])
+                          char weight_kernel)
 {
     int redSum = 0;
     int greenSum = 0;
     int blueSum = 0;
     int count = 0;
-    for (int j = -1; j <= 1; j++)
+
+    int (*kernel)[3][3] = &default_kernel;
+
+    for (int j = 0; j < 3; j++)
     {
-        int h = center_h + j;
+        int h = center_h + j - 1;
         if (h < 0 || h > height - 1)
         {
             continue;
         }
-        for (int i = -1; i <= 1; i++)
+        for (int i = 0; i < 3; i++)
         {
-            int w = center_w + i;
+            int w = center_w + i - 1;
             if (w < 0 || w > width - 1)
             {
                 continue;
             }
+            int my_little_kernel_value = (*kernel)[j][i];
             count += 1;
-            redSum += original_image[h][w].rgbtRed;
-            greenSum += original_image[h][w].rgbtGreen;
-            blueSum += original_image[h][w].rgbtBlue;
+            redSum += image[h][w].rgbtRed * (*kernel)[j][i];
+            greenSum += image[h][w].rgbtGreen * (*kernel)[j][i];
+            blueSum += image[h][w].rgbtBlue * (*kernel)[j][i];
         }
     }
 
@@ -92,6 +116,8 @@ RGBTRIPLE get_box_average(int height, int width, int center_h, int center_w, RGB
     average.rgbtBlue = round((float) blueSum / count);
     return average;
 }
+
+// void calculate_weighted_box_sum(int sum[3], )
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
@@ -125,4 +151,3 @@ BYTE clamp_color(int target)
     }
     return target;
 }
-
