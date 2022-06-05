e2c0e6da3347b0f0813418c1ac7f06ffc272c9ae
heyset <c.inaciomatheus@gmail.com>
2022-06-05 02:41:16 -0300

    week-4: recover: adds print usage reminder

--- week-4-memory/pset/recover/recover.c
+++ week-4-memory/pset/recover/recover.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void print_usage_reminder(void);
  
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        print_usage_reminder();
+        return 2;
+    }
+
+    char *filename = argv[1];
+}
+
+void print_usage_reminder(void)
+{
+    printf("Usage: recover FILE_PATH\n");
 }
