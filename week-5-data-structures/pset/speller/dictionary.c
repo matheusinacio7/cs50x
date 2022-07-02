a722b4716010c3ccae6d2b4d3165e08d73fa81c3
heyset <c.inaciomatheus@gmail.com>
2022-07-02 14:32:33 -0300

    week-5: speller: adds load, unload and size

--- week-5-data-structures/pset/speller/dictionary.c
+++ week-5-data-structures/pset/speller/dictionary.c
@@ -2,6 +2,9 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "dictionary.h"
 
@@ -13,11 +16,17 @@ typedef struct node
 }
 node;
 
+bool insert_word(char *word);
+bool insert_next(node *prev, char *word, int index);
+void free_node(node *n);
+
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
 
+int dict_word_count = 0;
+
 // Hash table
-node *table[N];
+node *table;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
@@ -36,20 +45,105 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    table = calloc(N, sizeof(node));
+    char c;
+    FILE *dict = fopen(dictionary, "r");
+
+    bool loaded = false;
+    int index = 0;
+    char word[LENGTH + 1];
+
+    while (fread(&c, sizeof(char), 1, dict))
+    {
+        if (c != '\n')
+        {
+            word[index] = c;
+            index++;
+        }
+        else
+        {
+            word[index] = '\0';
+            loaded = insert_word(word);
+
+            // if a word could not be loaded, break
+            if (!loaded)
+            {
+                break;
+            }
+
+            index = 0;
+            dict_word_count++;
+        }
+    }
+
+    fclose(dict);
+    free(table);
+    return loaded;
+}
+
+bool insert_word(char *word)
+{
+    int hash_index = hash(word);
+    bool inserted = false;
+
+    if (table[hash_index].word[0] == '\000')
+    {
+        strcpy(table[hash_index].word, word);
+        return true;
+    }
+    else
+    {
+        inserted = insert_next(&table[hash_index], word, hash_index);
+        return inserted;
+    }
+
+    return inserted;
+}
+
+bool insert_next(node *prev, char *word, int index)
+{
+    node *next = prev->next;
+    if (next == NULL)
+    {
+        node *new_node = calloc(1, sizeof(node));
+        if (!new_node)
+        {
+            return false;
+        }
+
+        strcpy(new_node->word, word);
+        prev->next = new_node;
+        return true;
+    }
+    else
+    {
+        return insert_next(next, word, index);
+    }
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return dict_word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
+    for (int i = 0; i < N; i++)
+    {
+        free_node(&table[i]);
+    }
     return false;
+}
+
+void free_node(node *n)
+{
+    if (n->next == NULL)
+    {
+        free(n);
+        return;
+    }
+
+    free_node(n->next);
 }
