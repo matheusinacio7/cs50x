2b9e2130f662b5346366e57a415916a1e4d77599
heyset <c.inaciomatheus@gmail.com>
2022-07-05 20:40:33 -0300

    week-6: mario-less: fixes linting

--- week-6-python/pset/sentimental-mario-less/mario.py
+++ week-6-python/pset/sentimental-mario-less/mario.py
@@ -1,1 +1,10 @@
-# TODO
+from cs50 import get_int
+
+while (True):
+    height = get_int("Height: ")
+    if (height > 0 and height < 9):
+        break
+
+for i in range(1, height + 1):
+    spaces = height - i
+    print(f"{' ' * spaces}{'#' * i}")
