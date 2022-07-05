c349e924e5593be6d037f1c0b88c0239960b6205
heyset <c.inaciomatheus@gmail.com>
2022-07-05 20:57:15 -0300

    week-6: cash: adds code

--- week-6-python/pset/sentimental-cash/cash.py
+++ week-6-python/pset/sentimental-cash/cash.py
@@ -1,1 +1,20 @@
-# TODO
+from cs50 import get_float
+
+while (True):
+    due = get_float("Change owed: ")
+    if (due > 0):
+        break
+
+cents = due * 100
+
+quarters = cents // 25
+cents -= quarters * 25
+
+dimes = cents // 10
+cents -= dimes * 10
+
+nickels = cents // 5
+cents -= nickels * 5
+
+pennies = cents
+print(int(quarters + dimes + nickels + pennies))
