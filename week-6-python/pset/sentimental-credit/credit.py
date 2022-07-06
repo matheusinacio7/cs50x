69a1e31f77d014671acba08b20050cbed967fd1d
heyset <c.inaciomatheus@gmail.com>
2022-07-05 21:14:54 -0300

    week-6: credit: adds check_if_valid

--- week-6-python/pset/sentimental-credit/credit.py
+++ week-6-python/pset/sentimental-credit/credit.py
@@ -1,1 +1,23 @@
-# TODO
+from cs50 import get_string
+
+def main():
+    number = get_string("Number: ")
+    print(check_if_valid(number))
+
+def check_if_valid(number):
+    digits = ''
+    for i in range(len(number) - 2, -1, -2):
+        double = int(number[i]) * 2
+        digits += str(double)
+    
+    for i in range(len(number) - 1, -1, -2):
+        digits += number[i]
+    
+    total_sum = sum([int(digit) for digit in list(digits)])
+    return total_sum % 10 == 0
+
+def check_if_visa(number):
+    if (len(number) not in [13, 16]):
+        return False
+
+main()
