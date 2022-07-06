b3c078ef7bdac5ee373e9a0215167d95c129e6b2
heyset <c.inaciomatheus@gmail.com>
2022-07-05 22:06:16 -0300

    week-6: readability: adds readability code

--- week-6-python/pset/sentimental-readability/readability.py
+++ week-6-python/pset/sentimental-readability/readability.py
@@ -1,12 +1,19 @@
 from cs50 import get_string
-
-def main():
-    text = get_string('Text: ')
-    # TODO get word count in text
-    # TODO get L (average of letters per 100 words)
-    # TODO get S (average of sentences per 100 words)
-    # TODO get index (index = 0.0588 * L - 0.296 * S - 15.8)
-    # TODO print index
+import re
 
 
-main()
+text = get_string('Text: ')
+word_count = len(text.split())
+letter_count = len([char for char in list(text) if char.isalpha()])
+sentences = re.split(r'[\.\!\?]+', text)
+sentences = list(filter(lambda s: s.strip() and len([char for char in list(s) if char.isalpha()]), sentences))
+sentence_count = len(sentences)
+L = letter_count / (word_count / 100)
+S = sentence_count / (word_count / 100)
+index = round(0.0588 * L - 0.296 * S - 15.8)
+if (index < 1):
+    print("Before Grade 1")
+elif (index > 15):
+    print("Grade 16+")
+else:
+    print(f"Grade {index}")
