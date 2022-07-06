45d4acc543688b978f5cf35bd0dc2525df2e0fc7
heyset <c.inaciomatheus@gmail.com>
2022-07-05 21:45:21 -0300

    week-6: readability: adds basic skeleton

--- week-6-python/pset/sentimental-readability/readability.py
+++ week-6-python/pset/sentimental-readability/readability.py
@@ -1,1 +1,12 @@
-# TODO
+from cs50 import get_string
+
+def main():
+    text = get_string('Text: ')
+    # TODO get word count in text
+    # TODO get L (average of letters per 100 words)
+    # TODO get S (average of sentences per 100 words)
+    # TODO get index (index = 0.0588 * L - 0.296 * S - 15.8)
+    # TODO print index
+
+
+main()
